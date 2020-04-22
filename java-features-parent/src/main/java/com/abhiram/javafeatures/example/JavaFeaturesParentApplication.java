f602b473930ba168b1e00bd772288c8e4388a3cc
pattabhia <shivaram2004@gmail.com>
2020-04-22 23:02:13 +0530

    adding parent
    adding parent

---
+++ java-features-parent/src/main/java/com/abhiram/javafeatures/example/JavaFeaturesParentApplication.java
@@ -0,0 +1,13 @@
+package com.abhiram.javafeatures.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFeaturesParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaFeaturesParentApplication.class, args);
+    }
+
+}
---
+++ java-features-parent/src/test/java/com/abhiram/javafeatures/example/JavaFeaturesParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhiram.javafeatures.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFeaturesParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
